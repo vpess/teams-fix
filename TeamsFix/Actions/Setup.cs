2ed92df3343b8c1ef840da7022e232f9b0aff356
Vinicius <vpess@protonmail.com>
2022-08-30 00:42:52 -0300

    Criação de novas classes.

---
+++ TeamsFix/Actions/Setup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace TeamsFix.Actions
+{
+    class Setup
+    {
+        private static void CheckTeams()
+        {
+            if (!Software.CheckInstallation("Microsoft Teams"))
+            {
+                MessageBox.Show("O Microsoft Teams não está instalado no sistema.", "Software nao encontrado", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MainWindow.Window.Close();
+            }
+        }
+
+        public static void Repair()
+        {
+            CheckTeams();
+
+            try
+            {
+                Teams.KillProcesses();
+                Teams.Repair();
+                MainWindow.Window.Message("Processo de reparo do Microsoft Teams concluído.");
+                Thread.Sleep(5000);
+                MainWindow.Window.Message("");
+            }
+            catch (Exception ex)
+            {
+                MainWindow.Window.Message("Ocorreu um erro no reparo do Teams.");
+                Logger.InsertInfo(ex.ToString());
+                throw;
+            }
+        }
+
+        public static void Reinstall()
+        {
+            CheckTeams();
+
+            try
+            {
+                Teams.KillProcesses();
+                Teams.Uninstall();
+                Teams.Install();
+                MainWindow.Window.Message("Processo de reinstalação do Microsoft Teams concluído.");
+                Thread.Sleep(5000);
+                MainWindow.Window.Message("");
+            }
+            catch (Exception ex)
+            {
+                MainWindow.Window.Message("Ocorreu um erro na reinstalação do Teams.");
+                Logger.InsertInfo(ex.ToString());
+                throw;
+            }
+
+        }
+    }
+}
---
+++ TeamsFix/Software.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace TeamsFix
+{
+    class Software
+    {
+        public static bool CheckInstallation(string software)
+        {
+            bool installed = false;
+
+            var HKLM64 = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, Environment.MachineName, RegistryView.Registry64);
+            RegistryKey softwaresKey64 = HKLM64.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+
+
+            foreach (string soft in softwaresKey64.GetSubKeyNames())
+            {
+                try
+                {
+                    var key = softwaresKey64.OpenSubKey(soft);
+                    var displayName = key.GetValue("DisplayName");
+
+                    if (displayName.ToString() != "")
+                    {
+                        if (displayName.ToString().ToUpper().Contains(software.ToUpper()))
+                        {
+                            installed = true;
+                            return installed;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            return installed;
+        }
+    }
+}
